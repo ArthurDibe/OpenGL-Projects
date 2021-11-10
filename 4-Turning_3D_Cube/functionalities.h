10563b35818ad20f36635955dd025620123aac78
Arthur Dibe <dibearthur@gmail.com>
2021-11-10 12:19:57 -0500

    Created header file for functionalities module

---
+++ 4-Turning_3D_Cube/functionalities.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONALITIES_H
+#define FUNCTIONALITIES_H
+
+#include "glm\glm.hpp"
+
+void drawCube(glm::mat4);
+void init(void);
+void display(void);
+void idle();
+void keyboard(unsigned char key, int x, int y);
+
+#endif
